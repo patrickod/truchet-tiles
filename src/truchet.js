fb4ea9d6e84a6fc9ed96178c0a4e0ac97ee25c93
Patrick O'Doherty <hello@patrickod.com>
2023-01-31 18:26:21 -0800

    Initial commit of randomly generated truchet tiles

---
+++ src/truchet.js
@@ -0,0 +1,35 @@
+window.addEventListener("DOMContentLoaded", (event) => {
+  var tiles = document.getElementsByClassName("truchet-cell");
+
+  var colors = ["black", "purple"];
+  var rotations = [0, 90, 180, 270];
+
+  for (var i = 0; i < tiles.length; i++) {
+    var colorI = Math.floor(Math.random() * colors.length);
+    var rotationI = Math.floor(Math.random() * rotations.length);
+
+    var tile = tiles[i];
+    var colorClass = colors[colorI];
+
+    var rotation = rotations[rotationI];
+    var rotationClass = "";
+    switch (rotation) {
+      case 0:
+        break;
+      case 90:
+        rotationClass = "rotate-90";
+        break;
+      case 180:
+        rotationClass = "rotate-180";
+        break;
+      case 270:
+        rotationClass = "rotate-270";
+        break;
+      default:
+        break;
+    }
+
+    // apply the style & rotation
+    tile.className = "truchet-cell " + colorClass + " " + rotationClass;
+  }
+});
